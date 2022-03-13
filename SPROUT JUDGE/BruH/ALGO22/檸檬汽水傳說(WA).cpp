64b912ed536d6ca419504967b888f9a1547167a1
TDDY <96487789+TDDY27@users.noreply.github.com>
2022-03-13 12:34:36 +0800

    Update and rename 檸檬汽水傳說(TLE).cpp to 檸檬汽水傳說(WA).cpp

--- SPROUT JUDGE/BruH/ALGO22/檸檬汽水傳說(WA).cpp
+++ SPROUT JUDGE/BruH/ALGO22/檸檬汽水傳說(WA).cpp
@@ -4,16 +4,17 @@
 #define TDDY ::ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
 using namespace std;
 
-vector <int> v;
-int t,n,a,ans;
+int t,n,a,ans,same1,same2,frnt;
 
 signed main(){
     TDDY
     cin>>t;
     while(t>0){
         cin>>n;
-        v.clear();
+        vector <int> v;
         ans=0;
+        same1=0;
+        same2=0;
 
         for(int i=0;i<n;i++){
             cin>>a;
@@ -23,9 +24,14 @@ signed main(){
                     ans++;
                     v.pop_back();
                     near++;
+                    if(a>same2)same1=0;
                 }else if(a==v.back()){
-                    for(int j=v.size()-1;v[j]==a;j--) ans++;
-                    if(v.size()>1)ans++;
+                    same2=a;
+                    if(same1==0) frnt=v.size();
+                    same1++;
+                    ans+=same1;
+                    if(v.size()>1 && frnt!=1) ans++;
+                    //cout<<same1<<endl;
                     break;
                 }else{
                     if(near==1)ans++;
