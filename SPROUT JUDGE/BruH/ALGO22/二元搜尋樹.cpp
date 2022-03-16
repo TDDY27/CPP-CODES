762f15713037093d6adf578cb8a423d6f8c25862
TDDY <96487789+TDDY27@users.noreply.github.com>
2022-03-16 23:34:56 +0800

    Update 二元搜尋樹.cpp

--- SPROUT JUDGE/BruH/ALGO22/二元搜尋樹.cpp
+++ SPROUT JUDGE/BruH/ALGO22/二元搜尋樹.cpp
@@ -1,1 +1,36 @@
+#include <bits/stdc++.h>
+#define TDDY ::ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+using namespace std;
+
+int const maxn=2500;
+int n,k;
+
+struct node{
+    int ind;
+    node *lc,*rc,*pr;
+
+    void printnode(node a){
+        cout<<a.ind<<endl;
+    }
+};
+
+signed main(){
+    TDDY
+    cin>>n;
+
+    node root,rec;
+    cin>>k;
+    root.ind=k;
+    root.pr=NULL;
+    rec.ind=k;
+
+    for(int i=0;i<n-1;i++){
+        cin>>k;
+        node temp;
+        if(k>temp)
+
+    }
+
+
+}
 
