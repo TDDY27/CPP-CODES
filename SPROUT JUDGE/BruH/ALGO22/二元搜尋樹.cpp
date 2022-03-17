1b3b54511fb00666ad89f12c9a27d55b2e250d7c
TDDY <96487789+TDDY27@users.noreply.github.com>
2022-03-18 00:34:29 +0800

    Update 二元搜尋樹.cpp

--- SPROUT JUDGE/BruH/ALGO22/二元搜尋樹.cpp
+++ SPROUT JUDGE/BruH/ALGO22/二元搜尋樹.cpp
@@ -1,5 +1,6 @@
 #include <bits/stdc++.h>
 #define TDDY ::ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define PB push_back
 using namespace std;
 
 int const maxn=2500;
@@ -8,29 +9,32 @@ int n,k;
 struct node{
     int ind;
     node *lc,*rc,*pr;
-
-    void printnode(node a){
-        cout<<a.ind<<endl;
-    }
 };
 
 signed main(){
     TDDY
     cin>>n;
 
-    node root,rec;
+    node now;
+    now.pr=NULL; now.lc=NULL; now.rc=NULL; now.ind=-1;
+    node root;
+
     cin>>k;
-    root.ind=k;
-    root.pr=NULL;
-    rec.ind=k;
+    root.ind=k; root.pr=NULL; root.lc=NULL; root.rc=NULL;
+    now=root;
 
-    for(int i=0;i<n-1;i++){
+    for(int i=0;i<n;i++){
         cin>>k;
-        node temp;
-        if(k>temp)
+        node *temp=new node;
+        temp->ind=k;
+        temp->lc=NULL;
+        temp->rc=NULL;
 
+        if(k<now.ind){
+            temp->pr=&now;
+            now.lc=temp;
+            now->lc; // 不知道出了甚麼bug
+        }else{
+        }
     }
-
-
 }
-
