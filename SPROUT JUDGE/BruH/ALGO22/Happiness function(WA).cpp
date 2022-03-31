e081f5cc45f088aeba95a2ee628974c7fb0f6851
TDDY <96487789+TDDY27@users.noreply.github.com>
2022-04-01 00:11:13 +0800

    Update Happiness function(WA).cpp

--- SPROUT JUDGE/BruH/ALGO22/Happiness function(WA).cpp
+++ SPROUT JUDGE/BruH/ALGO22/Happiness function(WA).cpp
@@ -1,47 +1,52 @@
 #include <bits/stdc++.h>
 #define TDDY ::ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
 #define PB push_back
-
+#define float double
 using namespace std;
 
 int t,n;
 float f[20][3];
 
-float func(int num,int ind){
-    return f[num][0]*(ind-f[num][1])*(ind-f[num][1])+f[num][2];
+float func(int ind){
+	float maxn=0;
+	for(int i=1;i<=n;i++) maxn=max( maxn, f[i][0]*(ind-f[i][1])*(ind-f[i][1])+f[i][2] ) ;
+    return maxn;
 }
 
-float ter(int a){
+float ter(){
+    int k=10000;
     float fa,fb,fc,fd;
     fa=0,fb=100,fc=200,fd=300;
-    while(fd-fa>1e-7){
-        cout<<func(a,fb)<<" "<<func(a,fc)<<endl;
-        if( func(a,fb)>func(a,fc) ){
+    while(k>0){
+        //cout<<fb<<" "<<fc<<endl;
+        if( func(fb)>func(fc) ){
             fa=fb;
             fc=fa+(fd-fa)*2/3;
             fb=fa+(fd-fa)*1/3;
-        }else if( func(a,fb)<func(a,fc) ){
+        }else if( func(fb)<func(fc) ){
             fd=fc;
             fc=fa+(fd-fa)*2/3;
             fb=fa+(fd-fa)*1/3;
-        }else if( func(a,fb)==func(a,fc)){
-            return func(a,(fb+fc)/2);
-        }
+		}else if( func(fb)==func(fc) ){
+            fa=fb;
+            fd=fc;
+            fc=fa+(fd-fa)*2/3;
+            fb=fa+(fd-fa)*1/3;
+		}
+		k--;
     }
-    return func(a,(fb+fc)/2);
+    return func((fb+fc)/2);
 }
 
 signed main(){
+	TDDY
     cin>>t;
     while(t>0){
         memset(f,0,sizeof(f));
 
         cin>>n;
         for(int i=1;i<=n;i++) cin>>f[i][0]>>f[i][1]>>f[i][2];
-        float maxn=-1;
-
-        for(int i=1;i<=n;i++) mmax=min(mmin,ter(i));
-        cout<<mmin<<endl;
+		cout<<setprecision(17)<<ter()<<endl;
 
         t--;
     }
