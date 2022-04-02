ea0083923e298a13972c91cf1d4588cdd32d2904
TDDY <96487789+TDDY27@users.noreply.github.com>
2022-04-02 14:56:17 +0800

    Update 1025.cpp

--- TIOJ/OK/1025.cpp
+++ TIOJ/OK/1025.cpp
@@ -9,24 +9,24 @@ string s;
 int G[15][15];
 
 int next(int a,int b){
-	for(int i=1;i<=9;i++)
-		for(int j=1;j<=9;j++)
-			if(G[i][j]==0 && i*100+j>a*100+b) return i*100+j;
-	return -1;
+    for(int i=1;i<=9;i++)
+        for(int j=1;j<=9;j++)
+    	    if(G[i][j]==0 && i*100+j>a*100+b) return i*100+j;
+    return -1;
 }
 
 int flag;
 void dfs(int a,int b){
    if(a==0 && b==-1){
       for(int i=1;i<=9;i++){
-    		for(int j=1;j<=9;j++)
-		    		cout<<G[i][j]<<" ";
-			cout<<endl;
-		}
-		cout<<endl;
-   	ans++;
-		return;
-	}
+    	  for(int j=1;j<=9;j++)
+	      cout<<G[i][j]<<" ";
+ 	      cout<<endl;
+	  }
+       cout<<endl;
+       ans++;
+       return;
+  }
   int temp[15];
   memset(temp,0,sizeof(temp))
   for(int i=1;i<=9;i++) temp[G[i][b]]=1;
@@ -34,29 +34,29 @@ void dfs(int a,int b){
   for(int i=( (a-1)/3)*3+1;i<( (a-1)/3)*3+4;i++)
         for(int j=( (b-1)/3)*3+1;j<( (b-1)/3)*3+4;j++)
             temp[G[i][j]]=1;
-	int x,y;
-	x=next(a,b)/100;
-	y=next(a,b)%100;
+  int x,y;
+  x=next(a,b)/100;
+  y=next(a,b)%100;
 
-	for(int i=1;i<=9;i++){
-		if(temp[i]==0){
-			G[a][b]=i;
-			dfs(x,y);
-		}
+  for(int i=1;i<=9;i++){
+	if(temp[i]==0){
+	    G[a][b]=i;
+  	    dfs(x,y);
 	}
-	G[a][b]=0;
+  }
+  G[a][b]=0;
 }
 
 signed main(){
-	TDDY
-	memset(G,0,sizeof(G));
-	for(int i=1;i<=9;i++)
-		for(int j=1;j<=9;j++) cin>>G[i][j];
+    TDDY
+    memset(G,0,sizeof(G));
+    for(int i=1;i<=9;i++)
+	for(int j=1;j<=9;j++) cin>>G[i][j];
 			
-	int sa,sb;
-	sa=next(0,0)/100;
-	sb=next(0,0)%100;
+    int sa,sb;
+    sa=next(0,0)/100;
+    sb=next(0,0)%100;
 
-	dfs(sa,sb);
-	cout<<"there are a total of "<<ans<<" solution(s).";
+    dfs(sa,sb);
+    cout<<"there are a total of "<<ans<<" solution(s).";
 }
