e66e439d572520e39c7b3dd91e4f07f8a3e4380c
TDDY <96487789+TDDY27@users.noreply.github.com>
2022-05-21 23:11:16 +0800

    Create 1662.cpp

---
+++ TIOJ/OK/1662.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#define TDDY ::ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+using namespace std;
+
+signed main(){
+    TDDY
+    string s;
+    while(getline(cin,s)){
+        int len=s.size();
+        for(int i=0;i<len;i++){
+            if(s[i]>='a' && s[i]<='z') s[i]=s[i]-'a'+'A';
+            else if(s[i]>='A' && s[i]<='Z') s[i]=s[i]-'A'+'a';
+            else s[i]=' ';
+        }
+        for(int i=0;i<len;i++) cout<<s[i];
+        cout<<endl;
+    }
+}
