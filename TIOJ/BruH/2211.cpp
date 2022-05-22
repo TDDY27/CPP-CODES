e92ddd446fd8a1bdf0001edf541fce1d4c707ac4
TDDY <96487789+TDDY27@users.noreply.github.com>
2022-05-22 16:55:53 +0800

    Update 2211.cpp

--- TIOJ/BruH/2211.cpp
+++ TIOJ/BruH/2211.cpp
@@ -3,15 +3,18 @@
 #define TDDY ::ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
 using namespace std;
 
+unordered_map <int,int> mp;
+
 signed main() {
     //TDDY
-    unordered_map <int,int> mp;
-    int n, k, tmp;
 
+	int n,k,tmp;
     n=Init();
     k=sqrt(n);
-    tmp=Query(1,1);
-    mp[tmp]=1;
+	
+	tmp=Query(1,1);
+	mp[tmp]=1;
+	if(tmp==1){ Report(1); return 0;}
 
     for(int i=2;i<=k;i++){
         tmp=Query(1,i);
@@ -20,12 +23,15 @@ signed main() {
     }
 
     int now,val;
-    now=(2*k+2)%n;
+    now=(k+1)%n;
+	if(now==0) now=n;
     while(true){
         val=Query(1,now);
         if(val==1) break;
         if(mp.count(val)){
-            now-=mp[val];
+			// 要注意因為now會％n所以now可能會比mp[val]小
+            if(now<=mp[val]) now+=n;
+			now-=mp[val];
             break;
         }
         now=(now+k)%n;
@@ -33,6 +39,9 @@ signed main() {
     }
 
     int ans;
-    ans=Query(1,now-1);
+	now--;
+	now=(now+n)%n;
+	if(now==0) now=n;
+    ans=Query(1,now);
     Report(ans);
 }
