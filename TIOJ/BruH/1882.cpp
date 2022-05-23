e19a2e8c5f5b6673d13e75cb873e39fb55fccce6
TDDY <96487789+TDDY27@users.noreply.github.com>
2022-05-23 16:23:27 +0800

    Create 1882.cpp

---
+++ TIOJ/BruH/1882.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+#define TDDY ::ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+using namespace std;
+
+int n;
+int f[10500][5];
+int mul=1e6;
+
+double func(double pos){
+    double mmin=1e8;
+    for(int i=1;i<=n;i++){
+        double tmp=f[i][1]*(pos*pos)+f[i][2]*pos+f[i][3];
+        mmin=min(mmin,tmp);
+    }
+    return mmin;
+}
+
+double ter(){ // ternary search
+    int t=1e4;
+    double f4,f3,f2,f1;
+    f4=60*24*90; f1=0; f3=(f4+f1)*2/3; f2=(f4+f1)/31;
+    for(int i=1;i<=t;i++){
+        if(func(f2)>func(f3)){
+            f4=f3;
+        }else if(func(f2)<func(f3)){
+            f1=f2;
+        }else if(func(f2)==func(f3)){
+            f4=f3; f1=f2;
+        }
+        f3=(f1+f4)*2/3; f2=(f1+f4)/3;
+        f1*=mul; f2*=mul; f3*=mul; f4*=mul;
+        f1=round(f1); f2=round(f2); f3=round(f3); f4=round(f4);
+        f1/=mul; f2/=mul; f3/=mul; f4/=mul;
+    }
+    double res=(f2+f3)/2;
+    res*=mul; res=round(res); res/=mul;
+    return res;
+}
+
+signed main(){
+    TDDY
+    cin>>n;
+    for(int i=1;i<=n;i++) cin>>f[i][1]>>f[i][2]>>f[i][3];
+    double ans=ter();
+    cout<<setprecision(6)<<fixed<<ans<<endl;
+}
