f5bc3b482e891bae57d3b5617059456cd005e94f
TDDY <96487789+TDDY27@users.noreply.github.com>
2022-05-24 10:30:31 +0800

    Update 1882.cpp

--- TIOJ/OK/1882.cpp
+++ TIOJ/OK/1882.cpp
@@ -7,7 +7,7 @@ int f[10500][5];
 double mul=1e6;
 
 double func(double pos){
-    double mmin=1e11;
+    double mmin=1e8;
     for(int i=1;i<=n;i++){
         double tmp=f[i][1]*(pos*pos)+f[i][2]*pos+f[i][3];
         mmin=min(mmin,tmp);
@@ -16,22 +16,14 @@ double func(double pos){
 }
 
 double ter(){ // ternary search
-    int t=1e4;
+    int t=75;
     double f4,f3,f2,f1;
     f4=60*24*90; f1=0; f3=(f1+f4+f4)/3; f2=(f1+f1+f4)/3;
 
     for(int i=1;i<=t;i++){
-        if(func(f2)>func(f3)){
-            f4=f3;
-        }else if (func(f2)<func(f3)){
-            f1=f2;
-        }else{
-            f4=f3; f1=f2;
-        }
+        if(func(f2)>func(f3)) f4=f3;
+        else f1=f2;
         f3=(f1+f4+f4)/3; f2=(f1+f1+f4)/3;
-        /*f1*=mul; f2*=mul; f3*=mul; f4*=mul;
-        f1=trunc(f1); f2=trunc(f2); f3=trunc(f3); f4=trunc(f4);
-        f1/=mul; f2/=mul; f3/=mul; f4/=mul;*/
     }
     double res=(f2+f3)/2;
     res*=mul; trunc(res); res/=mul;
