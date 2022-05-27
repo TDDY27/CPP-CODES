6760a1c3560dea221267cfb111304c4651cf5ad7
TDDY <96487789+TDDY27@users.noreply.github.com>
2022-05-27 19:53:50 +0800

    Create 1500(最近點對分治).cpp

---
+++ TIOJ/OK/1500(最近點對分治).cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+#define TDDY ::ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+
+#define int long long
+#define INF 9e18
+#define PB push_back
+#define pii pair<int,int>
+#define x first
+#define y second
+using namespace std;
+
+int n;
+vector< pii > pos;
+
+double dis(pii a,pii b){
+    double x2,y2;
+    x2=a.x-b.x; y2=a.y-b.y;
+    return sqrt(x2*x2+y2*y2);
+    /* double x2,y2;
+    x2=(a.x-b.x)*(a.x-b.x);
+    y2=(a.y-b.y)*(a.y-b.y);
+    return sqrt(x2+y2); 精度會不好，會WA*/
+}
+
+vector<pii> tmpos; //merge two arrange and sort by y
+double solve(int l,int r){
+    if(l>=r) return INF;
+    int mid=(l+r)/2 , midx=pos[mid].x;
+    double ans=min(solve(l,mid) , solve(mid+1,r));
+
+    tmpos.clear();
+    int tmp1=l,tmp2=mid+1;
+    for(int i=l;i<=r;i++){
+        if( (pos[tmp1].y<=pos[tmp2].y && tmp1<=mid ) || tmp2>r) tmpos.PB(pos[tmp1++]);
+        else tmpos.PB(pos[tmp2++]);
+    }
+
+    for(int i=0; i<(r-l+1);i++) pos[l+i]=tmpos[i];
+
+    tmpos.clear();
+    for(int i=l;i<=r;i++)
+        if( abs(pos[i].x-midx) <ans ) tmpos.PB(pos[i]);
+
+    for(int i=0;i<tmpos.size();i++){
+        for(int j=i+1;j<tmpos.size();j++){
+            if( abs(tmpos[j].y-tmpos[i].y) >ans) break;
+            ans=min(ans, dis(tmpos[i],tmpos[j]));
+        }
+    }
+    return ans;
+}
+
+signed main(){
+    TDDY
+    while(cin>>n){
+        pos.clear();
+        for(int i=0;i<n;i++){
+            int tmpx,tmpy;
+            cin>>tmpx>>tmpy;
+            pos.PB({tmpx,tmpy});
+        }
+
+        sort(pos.begin() ,pos.end());
+        double res;
+        res=solve(0,n-1);
+        cout<<fixed<<setprecision(6)<<res<<endl;
+    }
+}
+
