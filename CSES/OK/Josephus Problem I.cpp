a51a059ea13f9ed952782357eaf5297989c26d7b
TDDY <96487789+TDDY27@users.noreply.github.com>
2022-08-29 16:45:22 +0800

    Create Josephus Problem I.cpp

---
+++ CSES/OK/Josephus Problem I.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int const maxn=2e5+500;
+int bit[maxn];
+
+void update(int pos,int val){
+    for(int i=pos;i<maxn;i+=i&(-i) ) bit[i]+=val;
+}
+
+int query(int pos){
+    int sum=0;
+    for(int i=pos;i>=1;i-=i&(-i) ) sum+=bit[i];
+    return sum;
+}
+
+int who(int pos){
+    int sum=0, per=0;
+    for(int i=1<<17; i>=1; i>>=1 ){
+        if(per+i<maxn && sum+bit[per+i]<pos){ sum+=bit[i], per+=i; }
+    }
+    return per+1;
+}
+
+
+signed main(){
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++) update(i,1);
+
+    int now=0;
+    for(int i=1;i<=n;i++){
+        now=who( (query(now)+1)%query(n)+1 );
+        cout<<now<<" ";
+        update(now,-1);
+    }
+}
